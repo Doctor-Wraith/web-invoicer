198815855c6c71d1d0aa2eed316b72715bddf8c6
Thomas van Steen <thomas@webthomas.com>
2026-04-21 10:01:09 +0200

    Animation to tab selector

--- src/tabs/Tabs.tsx
+++ src/tabs/Tabs.tsx
@@ -5,6 +5,7 @@ import PaymentTab from "./PaymentTab"
 import CompanyTab from "./CompanyTab"
 import ProductSettings from "./ProductSettings.tsx";
 import "../styles/Tabs.css"
+import {useEffect, useRef, useState} from "react";
 
 const TABS = ["Products", "Settings", "Customer", "Invoice", "Payment", "Company"]
 
@@ -12,12 +13,29 @@ const TABS = ["Products", "Settings", "Customer", "Invoice", "Payment", "Company
 // @ts-expect-error
 export default function Tabs({activeTab, setActiveTab}) {
 
+    const [indicatorStyle, setIndicatorStyle] = useState({});
+    const tabRefs = useRef({});
+    
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        const activeEl = tabRefs.current[activeTab];
+        if (activeEl) {
+            setIndicatorStyle({
+                width: activeEl.clientWidth,
+                transform: `translateX(${activeEl.offsetLeft}px)`
+            })
+        }
+    }, [activeTab])
+    
     return (
         <div className="tab-wrapper">
             <div className="tabs">
+                <div className="tab-indicator" style={indicatorStyle}/>
                 {TABS.map(tab => (
                     <button
                     key={tab}
+                    ref={el => (tabRefs.current[tab] = el)}
                     className={`tablinks${activeTab === tab ? " active" : ""}`}
                     onClick={() => setActiveTab(tab)}
                     >
