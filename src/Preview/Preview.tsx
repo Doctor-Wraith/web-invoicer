7cf7040ac2f0d0cf4f74fd774190eb30d74353fe
Doctor Wraith <thomas.vansteen2004@gmail.com>
2026-05-03 12:14:00 +0200

    Added Proper Scrolling

--- src/Preview/Preview.tsx
+++ src/Preview/Preview.tsx
@@ -24,80 +24,87 @@ export default function Preview({products, services, currency, customer, invoice
 
 
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", overflow: "hidden" }}>
             <div className="title-card">
                 <h2>Preview</h2>
             </div>
-            {/*Header Thing*/}
-            <div className="Preview-header" style={{display: "inline-flex", justifyContent:"space-between", width:"100%"}}>
-                <PreviewCompany />
-                <PreviewInvoiceDetails invoiceDetails={invoiceDetails}/>
-            </div>
-
-            <hr/>
-
-            {/* Customer */}
-            <div className="customer-preview">
-                <label>Issued to:</label><br/>
-                <label>{customer.name ? customer.name : "Customer Name"}</label><br/>
-                {(customer.id ) && (
-                    <label>ID: {customer.id}<br/></label>
-                )}
-                {customer.address && (
-                    <label>Address: {customer.address}<br/></label>
-                )}
-                {customer.zip && (
-                    <label>Zip: {customer.zip}<br/></label>
-                )}
-                {customer.phone && (
-                    <label>Phone: {customer.phone}<br/></label>
-                )}
-                {customer.email && (
-                    <label>Email: {customer.email}<br/></label>
-                )}
-                <hr style={{ color: "rgba(255,255,255,0.5)"}}></hr>
-            </div>
-
-            {/*Products and services*/}
-            <div className="preview-table">
-                {(products.length > 0) && (
-                    <section>
-                        <PreviewProducts
-                            currency={currency}
-                            items={products}
-                            mode="product"
-                        />
-                    </section>
-                )}
-                {(services.length > 0) && (
-                    <section>
-                        <PreviewProducts
-                            currency={currency}
-                            items={services}
-                            mode="service"
-                        />
-                    </section>
-                )}
-
-                {(services.length === 0) && (products.length === 0) && (
-                    <section style={{textAlign: "center", width: "100%"}}>
-                        <h4 style={{alignItems: "center", width: "100%"}}>No Products or services</h4>
-                        <hr style={{ color: "rgba(255,255,255,0.5)"}}></hr>
-                    </section>
-                )}
-            </div>
-
-            {/*Totals*/}
-            {(subTotal > 0) && (
-                <div className="totaling-wrapper">
-                    <Total
-                        subTotal={subTotal}
-                        discount={invoiceDetails.discount}
-                        shipping={invoiceDetails.shipping}
-                        tax={invoiceDetails.tax}
-                        currency={currency}
-                    />
+            <div className="preview-information" style={{overflowY: "auto", padding: "5px", flex:1}}>
+                {/*Header Thing*/}
+                <div className="Preview-header"
+                     style={{display: "inline-flex", justifyContent: "space-between", width: "100%"}}>
+                    <PreviewCompany/>
+                    <PreviewInvoiceDetails invoiceDetails={invoiceDetails}/>
                 </div>
-            )}
+
+                <hr/>
+
+                {/* Customer */}
+                <div className="customer-preview">
+                    <label>Issued to:</label><br/>
+                    <label>{customer.name ? customer.name : "Customer Name"}</label><br/>
+                    {(customer.id) && (
+                        <label>ID: {customer.id}<br/></label>
+                    )}
+                    {customer.address && (
+                        <label>Address: {customer.address}<br/></label>
+                    )}
+                    {customer.zip && (
+                        <label>Zip: {customer.zip}<br/></label>
+                    )}
+                    {customer.phone && (
+                        <label>Phone: {customer.phone}<br/></label>
+                    )}
+                    {customer.email && (
+                        <label>Email: {customer.email}<br/></label>
+                    )}
+                    <hr style={{color: "rgba(255,255,255,0.5)"}}></hr>
+                </div>
+
+                {/*Products and services*/}
+                <div className="preview-table">
+                    {(products.length > 0) && (
+                        <section>
+                            <PreviewProducts
+                                currency={currency}
+                                items={products}
+                                mode="product"
+                            />
+                        </section>
+                    )}
+                    {(services.length > 0) && (
+                        <section>
+                            <PreviewProducts
+                                currency={currency}
+                                items={services}
+                                mode="service"
+                            />
+                        </section>
+                    )}
+
+                    {(services.length === 0) && (products.length === 0) && (
+                        <section style={{textAlign: "center", width: "100%"}}>
+                            <h4 style={{alignItems: "center", width: "100%"}}>No Products or services</h4>
+                            <hr style={{color: "rgba(255,255,255,0.5)"}}></hr>
+                        </section>
+                    )}
+                </div>
+
+                {/*Totals*/}
+                {(subTotal > 0) && (
+                    <div className="totaling-wrapper">
+                        <Total
+                            subTotal={subTotal}
+                            discount={invoiceDetails.discount}
+                            shipping={invoiceDetails.shipping}
+                            tax={invoiceDetails.tax}
+                            currency={currency}
+                        />
+                    </div>
+                )}
+
+                {(invoiceDetails) && (invoiceDetails.endMessage !== "") && (
+                    <label>{invoiceDetails.endMessage}</label>
+                )}
+            </div>
         </div>)
 }
